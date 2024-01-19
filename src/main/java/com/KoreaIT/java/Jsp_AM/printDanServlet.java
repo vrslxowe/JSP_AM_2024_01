ef151229e2f4ca318b348d60a79f177b05cc9dba
vrslxowe <vrslxowe@gmail.com>
2024-01-19 16:34:05 +0900

    구구단 기초

--- src/main/java/com/KoreaIT/java/Jsp_AM/printDanServlet.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/printDanServlet.java
@@ -1,22 +1,28 @@
 package com.KoreaIT.java.Jsp_AM;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-@WebServlet("/home/main2")
-public class HomeMainServlet2 extends HttpServlet {
-	
+@WebServlet("/printDan")
+public class printDanServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.getWriter().append("Hello world!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!").append(request.getContextPath());
-		
-		
-	}
 
+		response.setContentType("text/html;charset=UTF-8");
+
+		response.getWriter().append("==8단==<br>");
+
+		int dan = 8;
+
+		for (int i = 1; i <= 9; i++) {
+			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
+		}
+	}
 
 }
