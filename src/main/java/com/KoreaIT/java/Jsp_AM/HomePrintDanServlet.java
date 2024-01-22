8294317842e341431d77a9f2185de16617f8cf1a
vrslxowe <vrslxowe@gmail.com>
2024-01-22 09:55:01 +0900

    서블릿이 JSP 파일에게 요청정보를 넘겨줘서 협업이 가능하도록

--- src/main/java/com/KoreaIT/java/Jsp_AM/HomePrintDanServlet.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/HomePrintDanServlet.java
@@ -1,22 +1,19 @@
 package com.KoreaIT.java.Jsp_AM;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-@WebServlet("/home/main2")
-public class HomeMainServlet2 extends HttpServlet {
-	
+@WebServlet("/home/printDan")
+public class HomePrintDanServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.getWriter().append("Hello world!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!").append(request.getContextPath());
-		
-		
+		request.getRequestDispatcher("/jsp/home/printDan.jsp").forward(request, response);
 	}
-
 
 }
