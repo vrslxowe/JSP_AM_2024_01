19fa9b08c72c77deae2e85b9ea1b53eaa53db18f
vrslxowe <vrslxowe@gmail.com>
2024-01-23 13:52:49 +0900

    게시물 작성 기능 구현

---
+++ src/main/java/com/KoreaIT/java/Jsp_AM/servlet/ArticleModifyServlet.java
@@ -0,0 +1,82 @@
+package com.KoreaIT.java.Jsp_AM.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import com.KoreaIT.java.Jsp_AM.util.DBUtil;
+import com.KoreaIT.java.Jsp_AM.util.SecSql;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/article/modify")
+public class ArticleModifyServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		// DB연결
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("클래스가 없습니다.");
+			e.printStackTrace();
+		}
+		
+		String url = "jdbc:mysql://127.0.0.1:3306/JSP_AM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+		String user = "root";
+		String password = "";
+		
+		Connection conn = null;
+		
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+
+			int page = 1;
+
+			if (request.getParameter("page") != null && request.getParameter("page").length() != 0) {
+				page = Integer.parseInt(request.getParameter("page"));
+			}
+
+			int itemsInAPage = 10;
+			int limitFrom = (page - 1) * itemsInAPage;
+			
+			SecSql sql = SecSql.from("SELECT COUNT(*) AS cnt");
+			sql.append("FROM article");
+			
+			int totalCnt = DBUtil.selectRowIntValue(conn, sql);
+			int totalPage = (int) Math.ceil(totalCnt / (double) itemsInAPage);
+			
+			sql = SecSql.from("SELECT *");
+			sql.append("FROM article");
+			sql.append("ORDER BY id DESC");
+			sql.append("LIMIT ?, ?;", limitFrom, itemsInAPage);
+			
+			List<Map<String, Object>> articleRows = DBUtil.selectRows(conn, sql);
+			
+			request.setAttribute("page", page);
+			request.setAttribute("totalPage", totalPage);
+			request.setAttribute("articleRows", articleRows);
+			request.getRequestDispatcher("/jsp/article/modify.jsp").forward(request, response);
+			
+		} catch (SQLException e) {
+			System.out.println("에러 : " + e);
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
